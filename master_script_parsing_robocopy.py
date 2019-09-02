6da3a4bd9395960318430eb45885d15ec8fd7fd5
teddy20060022 <teddy.20060022@gmail.com>
2019-09-02 15:18:44 +0700

    Update master_script_parsing_robocopy.py

--- master_script_parsing_robocopy.py
+++ master_script_parsing_robocopy.py
@@ -4,12 +4,17 @@ Created on Fri Aug 30 09:52:44 2019
 
 @author: MuhammadSoSis
 """
+from datetime import date 
 #import re
-import cx_Oracle
+#import cx_Oracle
 import os
 os.chdir("C:\\Program Files\\Oracle\\instantclient_19_3")
 
-file_name = '20190805_backup.log'
+myDate = date.today()
+tanggal='{:%Y%m%d}'.format(myDate)
+#print(tanggal)
+
+file_name = '{}_backup.log'.format(tanggal)
 path_name = 'D:\\Project\\BSI\\BACKUP\\{}'.format(file_name)
 
 #print('Files : *PDBSTGACACON01*.*')
@@ -132,94 +137,3 @@ finally:
         con.close()
 """
 print('====================================================================')
-
-"""
-string_start = re.search('Files : *PDBSTGACACON01*.*', str(x))
-print(string_start)
-flag2 = index2 = 0
-for baris3 in x:
-    baris3.strip().split('\n')
-    index2+=1
-    if 'Started : ' in baris3:
-       flag2 = 1
-       break
-
-if flag2 == 0:
-   print(f"Sorry couldn't find {string_start}")
-else:
-   print(f'Found1 {string_start} in line {index2}')
-
-for y, lines in enumerate(x):
-    if y == index2 :
-        start_time = lines.split('Started : ')[1]
-        print(start_time)
-
-"""
-
-
-
-
-
-#===============================================================================================
-"""log_file_path = 'D:\\Project\\BSI\\BACKUP\\20190805_backup.log'
-start1 = 'start data transfer C:\\symscripts\\1_4_1_Transfer.bat PDBSTGACACON01'
-end1 = 'completed C:\\symscripts\\1_4_1_Transfer.bat PDBSTGACACON01'
-
-block_of_lines = []
-index=0
-with open(log_file_path,'r') as input_data:
-    # Skips text before the beginning of the interesting block:
-    for baris in (input_data.readlines()):
-        baris.strip().split('\n')
-        #print(baris)
-        index+=1
-        print(index)
-        if '{}'.format(start1) in baris:
-           flag = 1
-           break
-              
-    for i,line in enumerate(input_data.readlines()):
-        line.strip().split('\n')
-        if i > index: break
-        print(line)
-        #if '{}'.format(end1) in line:  # Or whatever test is needed
-         #   break  
-"""
-
-"""
-block_of_line = []
-data_file = open(log_file_path, 'r')
-block = ""
-found = False
-for line in data_file:
-    if found:
-        block += line
-        if line.strip() == '{}'.format(end1): break
-    else:
-        if line.strip() == '{}'.format(start1):
-            found = True
-            block = "Start"
-
-block_of_line.append(line)
-print(line)
-data_file.close()
-"""
-
-"""
-started = False
-collected_lines = []
-with open(log_file_path, 'r') as fp:
-     for i, line in enumerate(fp.readlines()):
-         if '{}'.format(start1) in line.strip():
-             started = True
-             print ('started at line', i) # counts from zero !
-             #print(line)
-             continue
-         if started and '{}'.format(end1) in line.strip() :
-             #print(line)
-             print ('end at line', i)
-             break
-             # process line 
-             #collected_lines.append(line.rstrip())
-             #print(collected_lines)
-"""

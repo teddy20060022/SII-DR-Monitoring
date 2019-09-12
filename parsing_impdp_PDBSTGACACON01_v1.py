6715153b0b27388c2c380498e73460e14a3a0ea9
teddy20060022 <teddy.20060022@gmail.com>
2019-09-12 16:10:13 +0700

    Add files via upload

---
+++ parsing_impdp_PDBSTGACACON01_v1.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 16:33:45 2019
+
+@author: MuhammadSoSis
+"""
+import pathlib
+import cx_Oracle 
+import os
+from datetime import date 
+from datetime import datetime 
+import re
+
+os.chdir("C:\\Program Files\\Oracle\\instantclient_19_3")
+#os.chdir("C:\\instantclient_19_3")
+
+dateTimeObj = datetime.now()
+timestampStr1 = dateTimeObj.strftime("%a %b %d %H:%M:%S %Y")
+timestampStr2 = dateTimeObj.strftime("%a %b %d %Y")
+#print('Current Timestamp : ', timestampStr1)
+
+myDate = date.today()
+tanggal='{:%Y%m%d}'.format(myDate)
+#print(tanggal)
+
+#path_name = '\\\\172.24.2.36\\c$\\syncscripts\\logs\\'
+file_name = '{}_impdp_PDBSTGACACON01.log'.format(tanggal)
+path_name = 'D:\\Project\\BSI\\RESTORE\\'
+value='{}{}'.format(path_name,file_name)
+#print(value.format(file_name)) 
+
+#---file name exists or doesn't exisist---
+file = pathlib.Path("{}{}".format(path_name,file_name))
+if file.exists ():
+    #reading file
+    with open('{}'.format(value), 'r') as f:
+       data = f.readlines()
+       #print(data)
+    
+    #parsing Start Time
+    string_start = re.search('Import: ', str(data))
+    flag = index = 0
+     
+    for baris in data:
+        index+=1
+        if string_start.group() in baris:
+           flag = 1
+           break
+    
+    for x, lines in enumerate(data):
+        if x == index-1 :
+            start_time = lines.split('Production on ')[1]
+            print(start_time)
+    
+    #parsing Status
+    flag1 = index1 = 0
+    string_status = re.search('completed|Failed', str(data))
+    
+    for baris1 in data:
+        index1+=1
+        if string_status.group() in baris1:
+            flag1=1
+            break
+    
+    for x1, lines1 in enumerate(data):
+        if x1 == index1-1 :
+            status = lines.split('SYS\_IMPORT\_FULL\_[0-9][0-9]\" ')[0].split(' at')[0]
+            print(status)
+    """
+    #message
+    string_message = 'Starting '
+    flag2 = index2 = 0
+    for baris2 in data:
+        index2+=1
+        if string_message in baris2:
+           flag2 = 1
+           break
+    #print(index2)
+    with open('{}'.format(value), 'r') as fb:
+        message = fb.readlines()[index2:index2+5]
+    for z in message:
+        print (z)
+    fb.close()
+    """
+    #parsing end_time
+    s3 = "Job"
+    flag3 = index3 = 0
+     
+    for baris3 in data:
+        index3+=1
+        if s3 in baris3:
+           flag3 = 1
+           break
+    
+    for x3, lines3 in enumerate(data):
+        if x3 == index3-1 :
+            end_time = lines.split(' at ')[1]
+            print(end_time)
+    
+    #file_name
+    keyword_file_name = "Starting "
+    flag4 = index4 = 0
+     
+    for baris4 in data:
+        index4+=1
+        if keyword_file_name in baris4:
+           flag4 = 1
+           break
+    
+    for x4, lines4 in enumerate(data):
+        if x4 == index4-1 :
+            file_name = lines4.split(' system/********@STG03P directory=dbdumps ')[1].split(' ' )[0]
+            print (file_name)
+    """
+    #file_size
+    keyword_total_size = "Total estimation"
+    flag5 = index5 = 0
+     
+    for baris5 in data:
+        index5+=1
+        if keyword_total_size in baris5:
+           flag5 = 1
+           break
+    
+    for x5, lines5 in enumerate(data):
+        if x5 == index5-1 :
+            total_size = lines5.split('BLOCKS method: ')[1]
+            print (total_size)
+            """
+    
+
+    #insert into table oracle
+     
+    try:
+        con = cx_Oracle.connect('USER_DEMO/oracle@192.168.0.101/DB11G')
+        #con = cx_Oracle.connect('monitor/monitor@172.17.30.77/XE')
+        cur = con.cursor()
+        sql_command = 'insert into USER_DEMO.Monitoring VALUES(\'STA\',\'R\',2,\'PDBLCBE_ADMIN\',\'{}\',\'{}\',\'{} {}\',\'NULL\',\'{}\',\'NULL\',\'Process Complete\')'.format(start_time,status,timestampStr2,end_time,file_name)
+        cur.execute(sql_command)          
+        con.commit()       
+        print("value inserted successful") 
+        
+    except cx_Oracle.DatabaseError as e: 
+        print("There is a problem with Oracle", e) 
+    
+    finally: 
+        if cur: 
+            cur.close() 
+        if con: 
+            con.close()   
+    
+    f.close()
+    
+else:
+    print("File not exist")
+    try: 
+        con = cx_Oracle.connect('USER_DEMO/oracle@192.168.0.101/DB11G')
+        cur = con.cursor()
+        sql_command = 'insert into USER_DEMO.Monitoring VALUES(\'STA\',\'R\',2,\'PDBLCBE_ADMIN\',\'{}\',\'FAILED\',\'NULL\',\'NULL\',\'NULL\',\'NULL\',\'File not found\')'.format(timestampStr1)
+        cur.execute(sql_command)                          
+        con.commit()       
+        print("value inserted successful") 
+       
+    except cx_Oracle.DatabaseError as e: 
+        print("There is a problem with Oracle", e) 
+    
+    finally: 
+        if cur: 
+            cur.close() 
+        if con: 
+            con.close()
+    
---
+++ robocopy_PDBLCBE_ADMIN_v1.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 10:06:40 2019
+
+@author: MuhammadSoSis
+"""
+import pathlib
+import cx_Oracle
+import os
+from datetime import date
+from datetime import datetime
+import re
+
+os.chdir("C:\\Program Files\\Oracle\\instantclient_19_3")
+#os.chdir("C:\\instantclient_19_3")
+
+dateTimeObj = datetime.now()
+timestampStr1 = dateTimeObj.strftime("%a %b %d %H:%M:%S %Y")
+timestampStr2 = dateTimeObj.strftime("%a %b %d %Y")
+#print('Current Timestamp : ', timestampStr1)
+
+myDate = date.today()
+tanggal='{:%Y%m%d}'.format(myDate)
+#print(tanggal)
+
+file_name = '{}_backup.log'.format(tanggal)
+path_name = 'D:\\Project\\BSI\\BACKUP\\'
+value='{}{}'.format(path_name,file_name)
+#print(path_name)
+
+#---file name exists or doesn't exisist---
+file = pathlib.Path("{}{}".format(path_name,file_name))
+if file.exists ():
+    #reading file
+    with open('{}'.format(value), 'r') as f:
+       data = f.readlines()
+    
+    start_string = 'start data transfer C:\\symscripts\\1_4_1_Transfer.bat PDBLCBE_ADMIN'
+    end_string = 'completed C:\\symscripts\\1_4_1_Transfer.bat PDBLCBE_ADMIN'
+    flag = index = 0
+    for baris in data:
+        index+=1
+        if start_string in baris:
+           flag = 1
+           break
+    
+    flag1 = index1 = 0
+    for baris1 in data:
+        index1+=1
+        if '{}'.format(end_string) in baris1:
+            flag1 = 1
+            break
+    
+    with open('{}'.format(value), 'r') as fh:
+        data1 = fh.readlines()[index-1:index1]
+    
+    stringToMatch = 'Started : '
+    matchedLine = ''
+    for line in data1:
+    	if stringToMatch in line:
+    		matchedLine = line
+    		break
+    #print(matchedLine)
+    start_time = matchedLine.split('Started : ')[1]
+    print('Started : {}'.format(start_time))
+    
+    stringToMatch1 = 'Bytes : '
+    matchedLine1 = ''
+    for line1 in data1:
+    	if stringToMatch1 in line1:
+    		matchedLine1 = line1
+    		break
+    #print(matchedLine1)
+    x1 = re.search('\d\d\d\.\d\d\sm|\sg', matchedLine1)
+    ukuran=(x1.group())
+    #search_byte = matchedLine1.split('Bytes :  ')[1].split(' m   ')[0]
+    print('Bytes : {}'.format(ukuran))
+    
+    stringToMatch2 = 'Times :   '
+    matchedLine2 = ''
+    for line2 in data1:
+    	if stringToMatch2 in line2:
+    		matchedLine2 = line2
+    		break
+    #print(matchedLine)
+    search_times = matchedLine2.split('Times :   ')[1].split(' ')[0]
+    print('Times : {}'.format(search_times))
+    
+    stringToMatch3 = 'Ended : '
+    matchedLine3 = ''
+    for line3 in data1:
+    	if stringToMatch3 in line3:
+    		matchedLine3 = line3
+    		break
+    #print(matchedLine3)
+    end_time = matchedLine3.split('Ended : ')[1]
+    print('Ended : {}'.format(end_time))
+    
+    stringToMatch4 = 'Files : '
+    matchedLine4 = ''
+    for line4 in data1:
+    	if stringToMatch4 in line4:
+    		matchedLine4 = line4
+    		break
+    #print(matchedLine4)
+    file_name = matchedLine4.split('Files : ')[1]
+    print('File Name : {}'.format(file_name))
+    
+    stringToMatch5 = 'completed'
+    matchedLine5 = ''
+    for line5 in data1:
+    	if stringToMatch5 in line5:
+    		matchedLine5 = line5
+    		break
+    #print(matchedLine5)
+    status = matchedLine5.split('-------------------- ')[1].split(' ')[0]
+    print('Status : {}'.format(status))
+    
+    f.close()
+    fh.close()
+    
+    #insert into table oracle
+    
+    try: 
+        con = cx_Oracle.connect('USER_DEMO/oracle@192.168.0.101/DB11G')    
+        cur = con.cursor()
+        sql_command = 'insert into USER_DEMO.Monitoring VALUES(\'STB\',\'C\',1,\'PDBLCBE_ADMIN\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'Process Completed\')'.format(start_time,status,end_time,search_times,file_name,ukuran)
+        cur.execute(sql_command)          
+        con.commit()       
+        print("value inserted successful") 
+        
+    except cx_Oracle.DatabaseError as e: 
+        print("There is a problem with Oracle", e) 
+      
+    # by writing finally if any error occurs 
+    # then also we can close the all database operation 
+    
+    finally: 
+        if cur: 
+            cur.close() 
+        if con: 
+            con.close()
+    
+else:
+    print("File not exist")
+    
+    try:
+        con = cx_Oracle.connect('USER_DEMO/oracle@192.168.0.101/DB11G')    
+        cur = con.cursor()
+        sql_command = 'insert into USER_DEMO.Monitoring VALUES(\'STB\',\'C\',1,\'PDBLCBE_ADMIN\',\'{}\',\'FAILED\',\'NULL\',\'NULL\',\'NULL\',\'NULL\',\'File not found\')'.format(timestampStr1)
+        cur.execute(sql_command)                          
+        con.commit()       
+
+    except cx_Oracle.DatabaseError as e: 
+        print("There is a problem with Oracle", e) 
+    
+    finally: 
+        if cur: 
+            cur.close() 
+        if con: 
+            con.close()

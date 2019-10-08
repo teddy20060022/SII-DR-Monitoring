6b5ebeb691241b7cbc2bd0168a62fc16b36452d6
teddy20060022 <teddy.20060022@gmail.com>
2019-10-08 15:41:07 +0700

    Add files via upload

---
+++ connect_sql.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 10:41:43 2019
+
+@author: MuhammadSoSis
+"""
+#import pandas as pd
+import pyodbc 
+##########CONFINS##########
+try:
+    server ='192.168.138.133'
+    db ='MSSQLSERVER'
+    tcon ='yes'
+    usern ='drmonuser'
+    pwd ='P@ssw0rd234'
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+db+';UID='+usern+';PWD='+pwd+'')
+    sql = 'SELECT @@version'
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
+    for row in cursor:
+        print('row = %r' % (row,))
+except pyodbc.Error as ex:
+    print("There is a problem with :", ex)
+    sqlstate = ex.args[0]
+
+    
+#######OLSS#######
+try:
+    server ='192.168.140.26'
+    db ='MSSQLSERVER'
+    tcon ='yes'
+    usern ='drmonuser'
+    pwd ='P@ssw0rd234'
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+db+';UID='+usern+';PWD='+pwd+'')
+    sql = 'SELECT @@version'
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
+    for row in cursor:
+        print('row = %r' % (row,))
+except pyodbc.Error as ex:
+    print("There is a problem with :", ex)
+    """
+    sqlstate = ex.args[0]
+    if sqlstate == '08001':
+        print(r'error')
+        pass
+    """
+
+##########MONITORING##########
+try:
+    server ='172.17.30.77'
+    db ='DRMONITORING'
+    tcon ='yes'
+    usern ='qlik'
+    pwd ='P@ssw0rd23'
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+db+';UID='+usern+';PWD='+pwd+'')
+    sql = 'SELECT @@version'
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
+    for row in cursor:
+        print('row = %r' % (row,))
+    
+except pyodbc.Error as ex:
+    #sqlstate = ex.args[0]
+    print("There is a problem with :", ex)

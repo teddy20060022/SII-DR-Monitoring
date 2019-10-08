338417c4247407740a1429fb30629d6354c48015
teddy20060022 <teddy.20060022@gmail.com>
2019-10-08 17:02:35 +0700

    Update connect_sql.py

--- connect_sql.py
+++ connect_sql.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct  8 10:41:43 2019
-
-@author: MuhammadSoSis
+@author: Muhammad Tedi Sopyan
 """
 #import pandas as pd
-import pyodbc 
+import pyodbc
+
 ##########CONFINS##########
 try:
     server ='192.168.138.133'
-    db ='MSSQLSERVER'
+    db ='DRMON'
     tcon ='yes'
     usern ='drmonuser'
     pwd ='P@ssw0rd234'
@@ -18,16 +18,16 @@ try:
     cursor = cnxn.cursor()
     cursor.execute(sql)
     for row in cursor:
-        print('row = %r' % (row,))
+        print('CONFINS = %r' % (row,))
 except pyodbc.Error as ex:
-    print("There is a problem with :", ex)
+    print("There is a problem with CONFINS:", ex)
     sqlstate = ex.args[0]
 
-    
+
 #######OLSS#######
 try:
     server ='192.168.140.26'
-    db ='MSSQLSERVER'
+    db ='drmon'
     tcon ='yes'
     usern ='drmonuser'
     pwd ='P@ssw0rd234'
@@ -36,15 +36,15 @@ try:
     cursor = cnxn.cursor()
     cursor.execute(sql)
     for row in cursor:
-        print('row = %r' % (row,))
+        print('OLSS = %r' % (row,))
 except pyodbc.Error as ex:
-    print("There is a problem with :", ex)
-    """
+    print("There is a problem with OLSS:", ex)
+    
     sqlstate = ex.args[0]
     if sqlstate == '08001':
         print(r'error')
         pass
-    """
+    
 
 ##########MONITORING##########
 try:
@@ -58,8 +58,8 @@ try:
     cursor = cnxn.cursor()
     cursor.execute(sql)
     for row in cursor:
-        print('row = %r' % (row,))
+        print('MONITORING = %r' % (row,))
     
 except pyodbc.Error as ex:
     #sqlstate = ex.args[0]
-    print("There is a problem with :", ex)
+    print("There is a problem with MONITORING :", ex)
